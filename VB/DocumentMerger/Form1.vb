67873a34fcf84e64115affe19213dc9092d4a6ba
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DocumentMerger/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DocumentMerger.Helpers
+Imports DevExpress.XtraRichEdit.API.Native
+
+Namespace DocumentMerger
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim filenames As New List(Of String) (New String() {"Documents\FloatingObjects.rtf", "Documents\CharacterFormatting.rtf", "Documents\HeadersFooters.rtf"})
+
+			Dim mergedDoc As Document = DocumentsMerger.MergeDouments(filenames)
+			richEditControl1.RtfText = mergedDoc.RtfText
+		End Sub
+	End Class
+End Namespace
---
+++ VB/DocumentMerger/Helpers/DocumentsMerger.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.XtraRichEdit
+Imports System.Data
+Imports DevExpress.XtraRichEdit.API.Native
+
+Namespace DocumentMerger.Helpers
+	Public Class DocumentsMerger
+
+		Public Shared Function MergeDouments(ByVal filenames As List(Of String)) As Document
+			Dim targetServer As New RichEditDocumentServer()
+			Dim sourceServer As New RichEditDocumentServer()
+			Dim targetDoc As Document = targetServer.Document
+			Dim sourceDoc As Document = sourceServer.Document
+
+			For i As Integer = 0 To filenames.Count - 1
+				sourceServer.LoadDocument(filenames(i))
+
+				targetDoc.Sections(targetDoc.Sections.Count - 1).UnlinkHeaderFromPrevious()
+				targetDoc.Sections(targetDoc.Sections.Count - 1).UnlinkFooterFromPrevious()
+
+				SectionsMerger.Append(sourceDoc, targetDoc)
+
+				If i = filenames.Count - 1 Then
+					Return targetDoc
+				End If
+
+				targetDoc.AppendSection()
+			Next i
+
+			Return targetDoc
+		End Function
+	End Class
+End Namespace
