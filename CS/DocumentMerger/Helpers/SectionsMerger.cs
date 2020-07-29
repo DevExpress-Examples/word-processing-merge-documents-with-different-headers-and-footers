f6cd45682aa0e10a3c20f3fedaeaa91e160d4785
Yulia <yuliya.samoylova@devexpress.com>
2020-07-29 18:51:50 +0300

    no message

--- CS/DocumentMerger/Helpers/SectionsMerger.cs
+++ CS/DocumentMerger/Helpers/SectionsMerger.cs
@@ -13,20 +13,26 @@ namespace DocumentMerger.Helpers {
                 Section sourceSection = source.Sections[i];
                 Section targetSection = target.Sections[lastSectionIndexBeforeAppending + i];
 
-                // Copy standard header/footer
+                // Copy header/footer
                 AppendHeader(sourceSection, targetSection, HeaderFooterType.Odd);
                 AppendFooter(sourceSection, targetSection, HeaderFooterType.Odd);
+                AppendHeader(sourceSection, targetSection, HeaderFooterType.Even);
+                AppendFooter(sourceSection, targetSection, HeaderFooterType.Even);
+                AppendHeader(sourceSection, targetSection, HeaderFooterType.First);
+                AppendFooter(sourceSection, targetSection, HeaderFooterType.First);
             }
         }
 
         private static void AppendHeader(Section sourceSection, Section targetSection, HeaderFooterType headerFooterType) {
+            if (!sourceSection.HasHeader(headerFooterType)) return;
+
             SubDocument source = sourceSection.BeginUpdateHeader(headerFooterType);
             SubDocument target = targetSection.BeginUpdateHeader(headerFooterType);
             target.Delete(target.Range);
             target.InsertDocumentContent(target.Range.Start, source.Range, InsertOptions.KeepSourceFormatting);
 
             // Delete empty paragraphs
-            DocumentRange emptyParagraph = target.CreateRange(target.Range.End.ToInt() - 2, 2);
+            DocumentRange emptyParagraph = target.CreateRange(target.Range.End.ToInt() - 1, 1);
             target.Delete(emptyParagraph);
 
             sourceSection.EndUpdateHeader(source);
@@ -34,13 +40,15 @@ namespace DocumentMerger.Helpers {
         }
 
         private static void AppendFooter(Section sourceSection, Section targetSection, HeaderFooterType headerFooterType) {
+            if (!sourceSection.HasFooter(headerFooterType)) return;
+
             SubDocument source = sourceSection.BeginUpdateFooter(headerFooterType);
             SubDocument target = targetSection.BeginUpdateFooter(headerFooterType);
             target.Delete(target.Range);
             target.InsertDocumentContent(target.Range.Start, source.Range, InsertOptions.KeepSourceFormatting);
             
             // Delete empty paragraphs
-            DocumentRange emptyParagraph = target.CreateRange(target.Range.End.ToInt() - 2, 2);
+            DocumentRange emptyParagraph = target.CreateRange(target.Range.End.ToInt() - 1, 1);
             target.Delete(emptyParagraph);
 
             sourceSection.EndUpdateFooter(source);
