895b2e7ddf1bac09a1caff9785e3406e24a9d4c0
DevExpressExampleBot <fakeEmail>
2023-10-08 15:12:23 +0400

    Source auto update [skip ci]

--- VB/DocumentMerger/Form1.vb
+++ VB/DocumentMerger/Form1.vb
@@ -1,27 +1,24 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports DocumentMerger.Helpers
 Imports DevExpress.XtraRichEdit.API.Native
 
 Namespace DocumentMerger
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			Dim filenames As New List(Of String) (New String() {"Documents\FloatingObjects.rtf", "Documents\CharacterFormatting.rtf", "Documents\HeadersFooters.rtf"})
+    Public Partial Class Form1
+        Inherits Form
 
-			Dim mergedDoc As Document = DocumentsMerger.MergeDouments(filenames)
-			richEditControl1.RtfText = mergedDoc.RtfText
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim filenames As List(Of String) = New List(Of String)() From {"Documents\FloatingObjects.rtf", "Documents\CharacterFormatting.rtf", "Documents\HeadersFooters.rtf"}
+            Dim mergedDoc As Document = DocumentsMerger.MergeDouments(filenames)
+            richEditControl1.RtfText = mergedDoc.RtfText
+        End Sub
+    End Class
 End Namespace
--- VB/DocumentMerger/Helpers/SectionsMerger.vb
+++ VB/DocumentMerger/Helpers/SectionsMerger.vb
@@ -1,52 +1,45 @@
-Imports Microsoft.VisualBasic
-Imports System
 Imports DevExpress.XtraRichEdit.API.Native
 
 Namespace DocumentMerger.Helpers
-	Public Class SectionsMerger
-		Public Shared Sub Append(ByVal source As Document, ByVal target As Document)
-			Dim lastSectionIndexBeforeAppending As Integer = target.Sections.Count - 1
-			Dim sourceSectionCount As Integer = source.Sections.Count
 
-			' Append document body
-			target.AppendRtfText(source.RtfText)
+    Public Class SectionsMerger
 
-			For i As Integer = 0 To sourceSectionCount - 1
-				Dim sourceSection As Section = source.Sections(i)
-				Dim targetSection As Section = target.Sections(lastSectionIndexBeforeAppending + i)
+        Public Shared Sub Append(ByVal source As Document, ByVal target As Document)
+            Dim lastSectionIndexBeforeAppending As Integer = target.Sections.Count - 1
+            Dim sourceSectionCount As Integer = source.Sections.Count
+            ' Append document body
+            target.AppendRtfText(source.RtfText)
+            For i As Integer = 0 To sourceSectionCount - 1
+                Dim sourceSection As Section = source.Sections(i)
+                Dim targetSection As Section = target.Sections(lastSectionIndexBeforeAppending + i)
+                ' Copy standard header/footer
+                Call AppendHeader(sourceSection, targetSection, HeaderFooterType.Odd)
+                Call AppendFooter(sourceSection, targetSection, HeaderFooterType.Odd)
+            Next
+        End Sub
 
-				' Copy standard header/footer
-				AppendHeader(sourceSection, targetSection, HeaderFooterType.Odd)
-				AppendFooter(sourceSection, targetSection, HeaderFooterType.Odd)
-			Next i
-		End Sub
+        Private Shared Sub AppendHeader(ByVal sourceSection As Section, ByVal targetSection As Section, ByVal headerFooterType As HeaderFooterType)
+            Dim source As SubDocument = sourceSection.BeginUpdateHeader(headerFooterType)
+            Dim target As SubDocument = targetSection.BeginUpdateHeader(headerFooterType)
+            target.Delete(target.Range)
+            target.InsertDocumentContent(target.Range.Start, source.Range, InsertOptions.KeepSourceFormatting)
+            ' Delete empty paragraphs
+            Dim emptyParagraph As DocumentRange = target.CreateRange(target.Range.End.ToInt() - 2, 2)
+            target.Delete(emptyParagraph)
+            sourceSection.EndUpdateHeader(source)
+            targetSection.EndUpdateHeader(target)
+        End Sub
 
-		Private Shared Sub AppendHeader(ByVal sourceSection As Section, ByVal targetSection As Section, ByVal headerFooterType As HeaderFooterType)
-			Dim source As SubDocument = sourceSection.BeginUpdateHeader(headerFooterType)
-			Dim target As SubDocument = targetSection.BeginUpdateHeader(headerFooterType)
-			target.Delete(target.Range)
-			target.InsertDocumentContent(target.Range.Start, source.Range, InsertOptions.KeepSourceFormatting)
-
-			' Delete empty paragraphs
-			Dim emptyParagraph As DocumentRange = target.CreateRange(target.Range.End.ToInt() - 2, 2)
-			target.Delete(emptyParagraph)
-
-			sourceSection.EndUpdateHeader(source)
-			targetSection.EndUpdateHeader(target)
-		End Sub
-
-		Private Shared Sub AppendFooter(ByVal sourceSection As Section, ByVal targetSection As Section, ByVal headerFooterType As HeaderFooterType)
-			Dim source As SubDocument = sourceSection.BeginUpdateFooter(headerFooterType)
-			Dim target As SubDocument = targetSection.BeginUpdateFooter(headerFooterType)
-			target.Delete(target.Range)
-			target.InsertDocumentContent(target.Range.Start, source.Range, InsertOptions.KeepSourceFormatting)
-
-			' Delete empty paragraphs
-			Dim emptyParagraph As DocumentRange = target.CreateRange(target.Range.End.ToInt() - 2, 2)
-			target.Delete(emptyParagraph)
-
-			sourceSection.EndUpdateFooter(source)
-			targetSection.EndUpdateFooter(target)
-		End Sub
-	End Class
+        Private Shared Sub AppendFooter(ByVal sourceSection As Section, ByVal targetSection As Section, ByVal headerFooterType As HeaderFooterType)
+            Dim source As SubDocument = sourceSection.BeginUpdateFooter(headerFooterType)
+            Dim target As SubDocument = targetSection.BeginUpdateFooter(headerFooterType)
+            target.Delete(target.Range)
+            target.InsertDocumentContent(target.Range.Start, source.Range, InsertOptions.KeepSourceFormatting)
+            ' Delete empty paragraphs
+            Dim emptyParagraph As DocumentRange = target.CreateRange(target.Range.End.ToInt() - 2, 2)
+            target.Delete(emptyParagraph)
+            sourceSection.EndUpdateFooter(source)
+            targetSection.EndUpdateFooter(target)
+        End Sub
+    End Class
 End Namespace
