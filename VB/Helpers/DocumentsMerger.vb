32671caf05e9223499e2bd7f29028afae956e76b
DevExpressExampleBot <fakeEmail>
2025-02-26 19:41:04 +0400

    autogenerate VB

--- VB/Helpers/DocumentsMerger.vb
+++ VB/Helpers/DocumentsMerger.vb
@@ -2,22 +2,20 @@ Imports DevExpress.XtraRichEdit
 Imports DevExpress.XtraRichEdit.API.Native
 
 Namespace DocumentMerger.Helpers
+
     Public Class DocumentsMerger
-        Public Shared Function MergeDocuments(filenames As List(Of String), outputFileName As String) As String
-            Dim targetServer As New RichEditDocumentServer()
-            Dim sourceServer As New RichEditDocumentServer()
+
+        Public Shared Function MergeDoсuments(ByVal filenames As List(Of String), ByVal outputFileName As String) As String
+            Dim targetServer As RichEditDocumentServer = New RichEditDocumentServer()
+            Dim sourceServer As RichEditDocumentServer = New RichEditDocumentServer()
             Dim targetDoc As Document = targetServer.Document
             Dim sourceDoc As Document = sourceServer.Document
-
             For i As Integer = 0 To filenames.Count - 1
                 sourceServer.LoadDocument(filenames(i))
-
                 targetDoc.Sections(targetDoc.Sections.Count - 1).UnlinkHeaderFromPrevious()
                 targetDoc.Sections(targetDoc.Sections.Count - 1).UnlinkFooterFromPrevious()
-
-                SectionsMerger.Append(sourceDoc, targetDoc)
-
-                If i = filenames.Count - 1 Then
+                Call SectionsMerger.Append(sourceDoc, targetDoc)
+                If i Is filenames.Count - 1 Then
                     targetServer.SaveDocument(outputFileName, DocumentFormat.OpenXml)
                     Return outputFileName
                 End If
--- VB/Program.vb
+++ VB/Program.vb
@@ -1,23 +1,18 @@
-Imports DocumentMerger.DocumentMerger.Helpers
+Imports DocumentMerger.Helpers
+Imports System.Diagnostics
 
 Namespace DocumentMerger
+
     Friend Class Program
+
         ''' <summary>
         ''' The main entry point for the application.
         ''' </summary>
-        Shared Sub Main(args As String())
-            Dim filenames As New List(Of String) From {
-                "Documents\FloatingObjects.rtf",
-                "Documents\CharacterFormatting.rtf",
-                "Documents\HeadersFooters.rtf"
-            }
-
-            Dim mergedDoc As String = DocumentsMerger.MergeDocuments(filenames, "Merged.docx")
-
-            Dim p As New Process()
-            p.StartInfo = New ProcessStartInfo("Merged.docx") With {
-                .UseShellExecute = True
-            }
+        Shared Sub Main(ByVal args As String())
+            Dim filenames As List(Of String) = New List(Of String)() From {"Documents\FloatingObjects.rtf", "Documents\CharacterFormatting.rtf", "Documents\HeadersFooters.rtf"}
+            Dim mergedDoc = DocumentsMerger.MergeDoсuments(filenames, "Merged.docx")
+            Dim p = New Process()
+            p.StartInfo = New ProcessStartInfo("Merged.docx") With {.UseShellExecute = True}
             p.Start()
         End Sub
     End Class
